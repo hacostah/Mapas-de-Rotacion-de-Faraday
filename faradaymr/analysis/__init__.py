3e93b0aaa5dffc1ffd2e27117f33e5338d6678d6
AngelEltrazo <angelmanuelriveracruz2006@gmail.com>
2026-09-12 21:13:04 -0700

    agregar minimos cuadrados para RM con diferenres lambdas

--- faradaymr/analysis/__init__.py
+++ faradaymr/analysis/__init__.py
@@ -1,3 +1,4 @@
+from .rm_fit import estimate_rm_lsq
 from .spatial_stats import radial_profile, transverse_rm_dispersion
 
-__all__ = ["radial_profile", "transverse_rm_dispersion"]
+__all__ = ["estimate_rm_lsq", "radial_profile", "transverse_rm_dispersion"]
